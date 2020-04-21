7cc8d542162bbf0b27663cf4ce1628a9126ef787
Alban Nabla <Alban@TKOPlaza.io>
2020-04-21 16:46:50 +0800

    added a few things to cylinder

--- Cylinder.py
+++ Cylinder.py
@@ -5,14 +5,14 @@ from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
 # create the side of the barrel
 alpha = np.linspace(0, 2*np.pi, 50)
-x = np.zeros([50,50])
-y = np.zeros([50,50])
-height = np.zeros([50,50])
-for i in range(len(x)):
+x = np.zeros([2,50])
+y = np.zeros([2,50])
+height = np.zeros([2,50])
+height[1] = np.ones(50)
+for i in range(len(height)):
     x[i] = np.cos(alpha)
     y[i] = np.sin(alpha)
-    height[i] = i/(len(x))
-    
+
 # create the top and bottom
 z = [1,0]
 v_1 = []
@@ -26,6 +26,6 @@ fig = plt.figure()
 ax = fig.add_subplot((111),projection='3d')
 ax.plot_surface(x,y,height)
 c = Poly3DCollection([v_1,v_2], linewidths=1, alpha=1)
-c.set_facecolor("yellow") 
+c.set_facecolor(["yellow","red"]) 
 ax.add_collection3d(c)
 plt.show()
